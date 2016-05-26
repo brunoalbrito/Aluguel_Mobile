c94748496c850f9329438d99166a0d0ca1c8f304
Bruno Al. Brito <brunoalbuquerquebrito@outlook.com>
2016-05-26 19:38:24 -0300

    Criação do Contrutor Produto

--- ProjetoLocacao/src/br/com/mackenzie/model/Smartphone.java
+++ ProjetoLocacao/src/br/com/mackenzie/model/Smartphone.java
@@ -1,9 +1,18 @@
 package br.com.mackenzie.model;
 
-
+import java.util.Date;
 
 public class Smartphone extends Produto {
-	private double cameraResolucao;
-	private char tipoRede;
-	private boolean dualChip;
+
+    private double cameraResolucao;
+    private char tipoRede;
+    private boolean dualChip;
+
+    public Smartphone(String modeloProduto, SO sistemaOperacional, Date anoFabricacao, double telaPol, String telaResolucao,
+            String fabricante, double precoPorDia, boolean statusAlugado, String acessorios,double cameraResolucao,char tipoRede,boolean dualChip) {
+        super(modeloProduto, sistemaOperacional, anoFabricacao, telaPol, telaResolucao, fabricante, precoPorDia, statusAlugado, acessorios);
+        this.cameraResolucao = cameraResolucao;
+        this.tipoRede = tipoRede;
+        this.dualChip = dualChip;
+    }
 }

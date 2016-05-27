0660ce9e1572151dd9e340aa8790bcde041f63e2
rsg73626 <renan.rsg@hotmail.com>
2016-05-27 16:32:56 -0300

    Controle
    Aprimoramento da classe controle

--- ProjetoLocacao/src/br/com/mackenzie/model/Fisico.java
+++ ProjetoLocacao/src/br/com/mackenzie/model/Fisico.java
@@ -1,7 +1,5 @@
 package br.com.mackenzie.model;
 
-
-
 public class Fisico extends Cliente {
 	private int CPF;
 	private String nomeCompleto;
@@ -10,5 +8,13 @@ public class Fisico extends Cliente {
         super(endereco, telefone);
         this.nomeCompleto = nome;
         this.CPF = CPF;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
     }
 }
